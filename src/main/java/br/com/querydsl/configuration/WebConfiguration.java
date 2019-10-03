2f522db38e575991056572385e3b59b3da6ed15a
Raphael Garcia Moreira <raphagmoreira@gmail.com>
2019-10-03 20:25:02 -0300

    Implementacao do Jackson

--- src/main/java/br/com/querydsl/configuration/WebConfiguration.java
+++ src/main/java/br/com/querydsl/configuration/WebConfiguration.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
